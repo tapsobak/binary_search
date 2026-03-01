bf620d4ffabbc69ce717eaad13bbb0307b0a42dc
Nasser Tapsoba <nassertapsoba@meta.com>
2026-03-01 11:15:54 -0500

    Refactored the binary_search code and fix a bug related to highest_index.

--- binary_search.py
+++ binary_search.py
@@ -1,30 +1,24 @@
-import math
-
-
 def binary_search(sorted_list, target):
     lowest_index = 0
-    highest_index = len(sorted_list) - 0
+    highest_index = len(sorted_list) - 1
     while lowest_index <= highest_index:
-        mid_index = (highest_index + lowest_index) / 2
-        mid_index = math.floor(mid_index)  # round mid to the lower integer
+        mid_index = (highest_index + lowest_index) // 2
         if sorted_list[mid_index] == target:
-            return mid_index
+            return f"target: {target} - index: {mid_index}"
         else:
             if sorted_list[mid_index] < target:
                 lowest_index = mid_index + 1
-                mid_index = (highest_index + lowest_index) / 2
-                mid_index = math.floor(mid_index)  # round mid to the lower integer
-                if sorted_list[mid_index] == target:
-                    return mid_index
+
             else:
                 highest_index = mid_index - 1
-                mid_index = (highest_index + lowest_index) / 2
-                mid_index = math.floor(mid_index)  # round mid to the lower integer
-                if sorted_list[mid_index] == target:
-                    return mid_index
 
-    return -1
+    return f"target: {target} - index: -1"
 
 
 if __name__ == "__main__":
-    print(binary_search([12, 15, 22, 31, 45, 58, 63, 71, 89, 94], 5))
+    print(binary_search([12, 15, 22, 31, 45, 58, 63, 71, 89, 94], 12))
+    print(binary_search([12, 15, 22, 31, 45, 58, 63, 71, 89, 94], 58))
+    print(binary_search([12, 15, 22, 31, 45, 58, 63, 71, 89, 94], 94))
+    print(binary_search([12, 15, 22, 31, 45, 58, 63, 71, 89, 94], 2))
+    print(binary_search([], 2))
+    print(binary_search([1], -4))
